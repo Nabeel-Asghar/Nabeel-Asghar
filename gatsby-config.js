d6c0608f5c1af31b0584d06e830fac9186f53a8f
Nabeel Asghar <nabeel.asghar156@gmail.com>
2022-11-18 17:19:59 -0500

    config

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,16 +11,5 @@ module.exports = {
     "gatsby-transformer-sharp",
     "gatsby-plugin-scroll-reveal",
     "gatsby-plugin-breakpoints",
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: "Nabeel Asghar",
-        short_name: "Nabeel",
-        start_url: "/",
-        display: "minimal-ui",
-        icon: "src/images/icon.png",
-        crossOrigin: `use-credentials`,
-      },
-    },
   ],
 };
